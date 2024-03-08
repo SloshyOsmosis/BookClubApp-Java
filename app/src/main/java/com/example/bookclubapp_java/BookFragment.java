58eac883ee10cfa43a5054c727c6c85abefa9c3e
SloshyOsmosis <dherbert5229@gmail.com>
2024-03-08 13:39:53 +0000

    Added Fragment to the book fragment

--- app/src/main/java/com/example/bookclubapp_java/BookFragment.java
+++ app/src/main/java/com/example/bookclubapp_java/BookFragment.java
@@ -1,21 +1,64 @@
 package com.example.bookclubapp_java;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class BookFragment extends Fragment {
 
+    TextView shelf;
 
+    Button expRead, expReading, expWant;
+
+    ReadFragment readFragment = new ReadFragment();
+    ReadingFragment readingFragment = new ReadingFragment();
+    WantFragment wantFragment = new WantFragment();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_book, container, false);
+
+        expRead = view.findViewById(R.id.expandRead);
+        expReading = view.findViewById(R.id.expandReading);
+        expWant = view.findViewById(R.id.expandWant);
+
+        shelf = view.findViewById(R.id.shelves);
+        shelf.setPaintFlags(shelf.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        expRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fm = getActivity().getSupportFragmentManager().beginTransaction();
+                fm.replace(R.id.container,readFragment).commit();
+            }
+        });
+        expReading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fm = getActivity().getSupportFragmentManager().beginTransaction();
+                fm.replace(R.id.container,readingFragment).commit();
+            }
+        });
+        expWant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fm = getActivity().getSupportFragmentManager().beginTransaction();
+                fm.replace(R.id.container,wantFragment).commit();
+            }
+        });
+
+
+
+        return view;
     }
 }
